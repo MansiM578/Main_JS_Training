92eaf6e01aa300b2874e7d30d559efe8c15438cc
MansiM578 <mansi.m9289@gmail.com>
2023-06-19 17:37:52 +0530

    second commit

--- countDown_timer/script.js
+++ countDown_timer/script.js
@@ -1,1 +1,35 @@
-"use strict";
+// "use strict";
+var t = 0;
+function getTime() {
+  var x = document.getElementById("frm1");
+  hr = x.elements[0].value;
+  min = x.elements[1].value;
+
+  window.t = hr * 3600 + min * 60;
+  window.per = window.t;
+  startCountdown();
+}
+function startCountdown() {
+  var temp = window.t;
+  window.t = window.t - 1;
+  var h = Math.floor(temp / 3600);
+  var m = Math.floor((temp % 3600) / 60);
+  var s = Math.floor(temp - h * 3600 - m * 60);
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("countdown").innerHTML =
+    h + "hr:" + m + "min:" + s + "sec";
+
+  var t = setTimeout(timer, 1000);
+
+  if (temp < 0) {
+    clearInterval(t);
+    document.getElementById("countdown").innerHTML = "Countdown time completed";
+  }
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
