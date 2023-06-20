ef03e7d872e76127a6993f4cf8fa2cf761ed20ca
MansiM578 <mansi.m9289@gmail.com>
2023-06-20 13:08:02 +0530

    [added media to timer and made some changes to javascript, made timer and calculator responsive using media queries]

--- countDown_timer/script.js
+++ countDown_timer/script.js
@@ -1,15 +1,15 @@
 // "use strict";
 var t = 0;
-function getTime() {
-  var x = document.getElementById("frm1");
+function myFunction() {
+  var x = document.getElementById("myForm");
   hr = x.elements[0].value;
   min = x.elements[1].value;
 
   window.t = hr * 3600 + min * 60;
   window.per = window.t;
-  startCountdown();
+  timer();
 }
-function startCountdown() {
+function timer() {
   var temp = window.t;
   window.t = window.t - 1;
   var h = Math.floor(temp / 3600);
@@ -18,13 +18,21 @@ function startCountdown() {
   m = checkTime(m);
   s = checkTime(s);
   document.getElementById("countdown").innerHTML =
-    h + "hr:" + m + "min:" + s + "sec";
+    h + " hr: " + m + " min: " + s + " sec ";
+  document.getElementById("progress-bar").style.width =
+    (temp * 100) / window.per + "%";
 
   var t = setTimeout(timer, 1000);
 
   if (temp < 0) {
-    clearInterval(t);
-    document.getElementById("countdown").innerHTML = "Countdown time completed";
+    clearTimeout(t);
+    document.getElementById("countdown").innerHTML = "Countdown Time Out";
+    function playAlarm() {
+      var alarm = document.getElementById("timeOutSound");
+      alarm.play();
+    }
+
+    playAlarm();
   }
 }
 function checkTime(i) {
